15f6d8603d18064d50a3a98e8432435f675fc7d9
rossanthony <ross@velo33.com>
2015-03-14 22:02:52 +0000

    experiments with erland and racket

---
+++ erlang/counter.erl
@@ -0,0 +1,25 @@
+-module(counter).
+-export([run/0, counter/1]).
+ 
+run() ->
+    S = spawn(counter, counter, [0]),
+    send_msgs(S, 100000),
+    S.
+ 
+counter(Sum) ->
+    receive
+        value -> io:fwrite("Value is ~w~n", [Sum]);
+        {inc, Amount} -> counter(Sum+Amount)
+    end.
+ 
+send_msgs(_, 0) -> true;
+send_msgs(S, Count) ->
+    S ! {inc, 1},
+    send_msgs(S, Count-1).
+ 
+% Usage:
+%    1> c(counter).
+%    2> S = counter:run().
+%       ... Wait a bit until all children have run ...
+%    3> S ! value.
+%    Value is 100000
