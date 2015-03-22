2f25eb837ce4600a913ff9b14d448923d409e8bd
rossanthony <ross@velo33.com>
2015-03-22 22:53:40 +0000

    scala: setting up the grid for the cipher and testing

--- scala/Coder.scala
+++ scala/Coder.scala
@@ -1,32 +1,50 @@
 /**
  * Playfair Cipher
  * 
+ * @author Ross Anthony
  */
+
 import scala.io.StdIn._
 import java.io._
 import scala.io.Source
 import scala.language.postfixOps
-import scala.util.matching.Regex
+
 
 object Coder {
 	var keyword : String = "Pennsylvania"
+	var alphabet : String = "abcdefghiklmnopqrstuvwxyz" // excluding 'j'
+	  
+	//var Grid = scala.collection.mutable.Map[(Int, Int), Char]()
+	type Grid = Map[(Int, Int), Char]
 	
-	def main() {
-		prompt(readLine("encode, decode, or quit? "))
+	/**
+	 * The main constructor method, just run the prompt
+	 */
+	def main() = {
+		prompt()
 	}
 	
 	/**
-	 * Prompt the user for action
+	 * Prompt the user for action and try to run the command
 	 */
-	def prompt(arg : String) = (arg) match {
+	def prompt() : Unit = { 
+	  runCommand(readLine("encode, decode, or quit? ")) 
+	}
+	
+	/**
+	 * Run the command entered by the user (if it matches a valid command)
+	 * 
+	 * @param cmd: the command entered by the user
+	 */
+	def runCommand(cmd : String) = (cmd) match {
     case "encode" => println("encoding..."); encode()
 		case "decode" => println("decoding..."); decode()
-		case "quit"   => println("goodbye...")
-		case unknown => println("Unknown command '"+unknown+"' please try again..."); main() 
+		case "quit"   => println("goodbye...") // do nothing, exit
+		case unknown => println("Unknown command \"" + unknown + "\" please try again..."); prompt() 
 	}
 	
 	def retrieveKeyword() = {
-		keyword = readLine("Keyword: ")
+		keyword = sanitise(readLine("Keyword: "))
 		println(keyword)
 		keyword
 	}
@@ -51,9 +69,11 @@ object Coder {
 	
 	/** 
 	 * Sanitise
-	 *   	only allow alpha chars (strip out all punctuation and any digits)
-	 *  	replace j with i and convert to lowercase
-	 * @return String
+	 * 
+	 * only allow alpha chars (strip out all punctuation and any digits)
+	 * replace j with i and convert to lowercase
+	 * 
+	 * @return String: cleaned up
 	 */
   def sanitise(input: String) = {
     var sanitise = input.replaceAll("[^A-Za-z]", "") // remove all non-word chars
@@ -61,13 +81,53 @@ object Coder {
     sanitise.toLowerCase
   }
   
-	def encode() {
+  /**
+   * Construct the cipher grid 
+   * 
+   * 5 x 5 grid, consisting of a map of x y coordinates for each character (excluding
+   *     
+   * @return Grid
+   */
+  def renderGrid(keyword: String) : Grid = {
+    var gridChars = sanitise(keyword.concat(alphabet).distinct)
+    (for (x<-0 to 4; y<-0 to 4) yield (x,y)).zip(gridChars).toMap
+  }
+  
+  /**
+   * Testing the grid, output to console to check it's correct
+   */
+  def displayGrid(grid: Grid) = {
+    var output : String = ""
+    for (x<-0 to 4; y<-0 to 4){
+      output += grid(x,y) + " "
+      if(y > 3) output += "\n"
+    }
+    println(output)
+//    println(grid(0,0)+' '+grid(0,1)+' '+grid(0,2)+' '+grid(0,3)+' '+grid(0,4))
+//    println(grid(1,0))
+//    println(grid(2,0))
+//    println(grid(3,0))
+//    println(grid(4,0))
+  }
+  
+  /**
+   * The main encoder 
+   * 
+   * run whenever the user enters "encode"
+   * 
+   * @return File: encoded
+   */
+	def encode() = {
 	  var keyword = retrieveKeyword()
+	  println(keyword)
+	  
+	  var test = renderGrid(keyword)
+	  displayGrid(test)
+	  
 	  var fileContents = retrieveFile()
-	  println(sanitise(fileContents))
 	}
-	
-	def decode() {
+	  
+	def decode() = {
 	  
 	  retrieveKeyword()
 	  retrieveFile()
