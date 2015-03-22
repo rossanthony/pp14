58c9e2580d5edbc12af2d34ca13eee834d9d9c77
rossanthony <ross@velo33.com>
2015-03-22 17:24:05 +0000

    scala: adding sanitisation method for cleaning input strings

--- scala/Coder.scala
+++ scala/Coder.scala
@@ -5,7 +5,8 @@
 import scala.io.StdIn._
 import java.io._
 import scala.io.Source
-
+import scala.language.postfixOps
+import scala.util.matching.Regex
 
 object Coder {
 	var keyword : String = "Pennsylvania"
@@ -14,29 +15,28 @@ object Coder {
 		prompt(readLine("encode, decode, or quit? "))
 	}
 	
+	/**
+	 * Prompt the user for action
+	 */
 	def prompt(arg : String) = (arg) match {
-		case "encode" => println("encoding..."); encode()
+    case "encode" => println("encoding..."); encode()
 		case "decode" => println("decoding..."); decode()
-		case "quit" => println("goodbye...")
-		case _ => println("Unknown command, please try again..."); main() 
+		case "quit"   => println("goodbye...")
+		case unknown => println("Unknown command '"+unknown+"' please try again..."); main() 
 	}
 	
-	def retrieveKeyword() {
+	def retrieveKeyword() = {
 		keyword = readLine("Keyword: ")
 		println(keyword)
+		keyword
 	}
 	
-	def retrieveFile() {
-		val filename = readLine("Filename: ") 
+	def retrieveFile(): String = {
 		try { 
-			Source.fromFile(filename).getLines.mkString 
+			Source.fromFile(readLine("Filename: ")).getLines.mkString 
 		} catch {
-			case ioe: FileNotFoundException => println("File not found!")
-			case e: Exception => println("Unable to open file, check permissions.")
+			case e: Exception => ""
 		}
-//		println(lines)
-//		var test = 7+3
-		//return lines
 	}
 	
 	def writeFile() {
@@ -49,11 +49,22 @@ object Coder {
 		println(lines)
 	}
 	
+	/** 
+	 * Sanitise
+	 *   	only allow alpha chars (strip out all punctuation and any digits)
+	 *  	replace j with i and convert to lowercase
+	 * @return String
+	 */
+  def sanitise(input: String) = {
+    var sanitise = input.replaceAll("[^A-Za-z]", "") // remove all non-word chars
+    sanitise map (char => if (char == 'j') 'i' else char)
+    sanitise.toLowerCase
+  }
+  
 	def encode() {
-	  
-	  retrieveKeyword()
-	  var contents = retrieveFile()
-	  println(contents)
+	  var keyword = retrieveKeyword()
+	  var fileContents = retrieveFile()
+	  println(sanitise(fileContents))
 	}
 	
 	def decode() {
