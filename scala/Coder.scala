4fe754025373407381e3b6ffd95c668919292e82
rossanthony <ross@velo33.com>
2015-03-23 20:55:39 +0000

    cleaning up

--- scala/Coder.scala
+++ scala/Coder.scala
@@ -11,70 +11,70 @@ import scala.language.postfixOps
 
 
 object Coder {
-	var keyword : String = "Pennsylvania"
-	var alphabet : String = "abcdefghiklmnopqrstuvwxyz" // excluding 'j'
-	  
-	//var Grid = scala.collection.mutable.Map[(Int, Int), Char]()
-	type Grid = Map[(Int, Int), Char]
-	
-	/**
-	 * The main constructor method, just run the prompt
-	 */
-	def main() = {
-		prompt()
-	}
-	
-	/**
-	 * Prompt the user for action and try to run the command
-	 */
-	def prompt() : Unit = { 
-	  runCommand(readLine("encode, decode, or quit? ")) 
-	}
-	
-	/**
-	 * Run the command entered by the user (if it matches a valid command)
-	 * 
-	 * @param cmd: the command entered by the user
-	 */
-	def runCommand(cmd : String) = (cmd) match {
+  var keyword : String = "Pennsylvania"
+  var alphabet : String = "abcdefghiklmnopqrstuvwxyz" // excluding 'j'
+    
+  //var Grid = scala.collection.mutable.Map[(Int, Int), Char]()
+  type Grid = Map[(Int, Int), Char]
+  
+  /**
+   * The main constructor method, just run the prompt
+   */
+  def main() = {
+    prompt()
+  }
+  
+  /**
+   * Prompt the user for action and try to run the command
+   */
+  def prompt() : Unit = { 
+    runCommand(readLine("encode, decode, or quit? ")) 
+  }
+  
+  /**
+   * Run the command entered by the user (if it matches a valid command)
+   * 
+   * @param cmd: the command entered by the user
+   */
+  def runCommand(cmd : String) = (cmd) match {
     case "encode" => println("encoding..."); encode()
-		case "decode" => println("decoding..."); decode()
-		case "quit"   => println("goodbye...") // do nothing, exit
-		case unknown => println("Unknown command \"" + unknown + "\" please try again..."); prompt() 
-	}
-	
-	def retrieveKeyword() = {
-		keyword = sanitise(readLine("Keyword: "))
-		println(keyword)
-		keyword
-	}
-	
-	def retrieveFile(): String = {
-		try { 
-			Source.fromFile(readLine("Filename: ")).getLines.mkString 
-		} catch {
-			case e: Exception => ""
-		}
-	}
-	
-	def writeFile() {
-		val file = new File("output.txt")
-		val writer = new PrintWriter(file)
-		writer write "first\r\n"
-		writer write "second"
-		writer.close()
-		val lines = Source.fromFile(file).getLines().toList
-		println(lines)
-	}
-	
-	/** 
-	 * Sanitise
-	 * 
-	 * only allow alpha chars (strip out all punctuation and any digits)
-	 * replace j with i and convert to lowercase
-	 * 
-	 * @return String: cleaned up
-	 */
+    case "decode" => println("decoding..."); decode()
+    case "quit"   => println("goodbye...") // do nothing, exit
+    case unknown => println("Unknown command \"" + unknown + "\" please try again..."); prompt() 
+  }
+  
+  def retrieveKeyword() = {
+    keyword = sanitise(readLine("Keyword: "))
+    println(keyword)
+    keyword
+  }
+  
+  def retrieveFile(): String = {
+    try { 
+      Source.fromFile(readLine("Filename: ")).getLines.mkString 
+    } catch {
+      case e: Exception => ""
+    }
+  }
+  
+  def writeFile() {
+    val file = new File("output.txt")
+    val writer = new PrintWriter(file)
+    writer write "first\r\n"
+    writer write "second"
+    writer.close()
+    val lines = Source.fromFile(file).getLines().toList
+    println(lines)
+  }
+  
+  /** 
+   * Sanitise
+   * 
+   * only allow alpha chars (strip out all punctuation and any digits)
+   * replace j with i and convert to lowercase
+   * 
+   * @return String: cleaned up
+   */
   def sanitise(input: String) = {
     var sanitise = input.replaceAll("[^A-Za-z]", "") // remove all non-word chars
     sanitise map (char => if (char == 'j') 'i' else char)
@@ -103,11 +103,6 @@ object Coder {
       if(y > 3) output += "\n"
     }
     println(output)
-//    println(grid(0,0)+' '+grid(0,1)+' '+grid(0,2)+' '+grid(0,3)+' '+grid(0,4))
-//    println(grid(1,0))
-//    println(grid(2,0))
-//    println(grid(3,0))
-//    println(grid(4,0))
   }
   
   /**
@@ -117,20 +112,18 @@ object Coder {
    * 
    * @return File: encoded
    */
-	def encode() = {
-	  var keyword = retrieveKeyword()
-	  println(keyword)
-	  
-	  var test = renderGrid(keyword)
-	  displayGrid(test)
-	  
-	  var fileContents = retrieveFile()
-	}
-	  
-	def decode() = {
-	  
-	  retrieveKeyword()
-	  retrieveFile()
-	}
-	
+  def encode() = {
+    var keyword = retrieveKeyword()
+    println(keyword)
+    
+    var test = renderGrid(keyword)
+    displayGrid(test)
+    
+    var fileContents = retrieveFile()
+  }
+    
+  def decode() = {
+    retrieveKeyword()
+    retrieveFile()
+  }
 }
