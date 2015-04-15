221aac565943f18eac0659665921a7353d91ab3c
rossanthony <ross@velo33.com>
2015-04-15 23:24:02 +0100

    Prolog: setting up a predicate for printing out the maze grid

--- prolog/maze.pl
+++ prolog/maze.pl
@@ -1,14 +1,15 @@
+module(maze, [mazeSize/2, barrier/2]).
 mazeSize(5,9).
-barrier(1,8).
-barrier(2,1).
-barrier(2,2).
-barrier(2,4).
-barrier(2,5).
-barrier(3,4).
-barrier(3,7).
-barrier(3,9).
-barrier(4,4).
-barrier(4,7).
-barrier(4,8).
-barrier(4,9).
-barrier(5,2). 
+barrier(1, 8).
+barrier(2, 1).
+barrier(2, 2).
+barrier(2, 4).
+barrier(2, 5).
+barrier(3, 4).
+barrier(3, 7).
+barrier(3, 9).
+barrier(4, 4).
+barrier(4, 7).
+barrier(4, 8).
+barrier(4, 9).
+barrier(5, 2).
---
+++ prolog/paths.pl
@@ -0,0 +1,16 @@
+path(1,2).
+path(2,5).
+path(1,3).
+path(3,6).
+path(6,7).
+path(7,8).
+path(3,4).
+path(4,8).
+
+route(Start,End) :-
+	path(Start,End).
+
+route(Start,End) :-
+	path(Start,Somewhere),
+	route(Somewhere,End).
+
